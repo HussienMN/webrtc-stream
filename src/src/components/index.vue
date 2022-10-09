ff058af8b058a2be8d990a23eb7cf499da00aee0
Hussien Mnaizel <ko.sv@hotmail.com>
2022-10-09 03:58:10 +0300

    Add files via upload

---
+++ src/src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+        <webrtc-stream></webrtc-stream>
+	</div>
+</template>
+ 
+<script>
+	import Vue from 'vue';
+	import webrtcstream from '@bit/hussienmn.webrtc-streaming.webrtc';
+
+	Vue.component(webrtcstream.name, webrtcstream)
+
+	export default {}
+</script>
+<style scoped>
+
+.button {
+	margin: 5px;
+}
+</style>
